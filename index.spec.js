b41c5afb63b59e8c72603a79c2871fe4a0ad548c
Santiago Perez Perret <perezperret@gmail.com>
2018-12-06 15:41:24 -0400

    Simplify index spec

--- index.spec.js
+++ index.spec.js
@@ -1,97 +1,14 @@
-import nock from 'nock'
-import { mountFixture, unmountFixture } from './spec/utils'
-import * as FormKeep from './index'
+import FormKeep from './index'
 
 describe('index.js', () => {
-  const formFixture = `
-    <form id="test-form">
-      <div id="info-box"></div>
-      <input type="hidden" name="utf8" value="✓" />
-      <input name="email" value="test@example.com" />
-      <textarea name="comments">I ❤️ FormKeep</textarea>
-      <button id="test-button" type="submit">Submit</button>
-    </form>
-  `
-
-  const setUpForm = () => {
-    const form = document.getElementById('test-form')
-    const formkeepIdentifier = 'f3a748fed01a'
-    const config = {
-      beforeSubmit: (formJson) => {
-        formJson.datePosted = '2018-11-28'
-        return formJson
-      },
-      onSuccess: (response) => {
-        document.getElementById('info-box').textContent = 'Submitted successfully'
-      },
-      onFailure: (response) => {
-        document.getElementById('info-box').textContent = 'Failed to submit'
-      }
-    }
-    FormKeep.asyncForm(form, formkeepIdentifier, config)
-  }
-
-  const mockFormKeep = () => (
-    nock('https://formkeep.com')
-    .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
-    .post('/f/f3a748fed01a')
-  )
-
-  const clickSubmit = () => (
-    document
-    .getElementById('test-button')
-    .dispatchEvent(new Event('click'))
-  )
-
-  beforeEach(() => {
-    mountFixture('asyncForm', formFixture)
-    setUpForm()
-  })
-
-  afterEach(() => {
-    unmountFixture('asyncForm')
-  })
-
-  it('shows a success message when the form is submitted', (done) => {
-    const httpRequestMock = mockFormKeep().reply(200, 'OK')
-
-    clickSubmit()
-
-    setTimeout(() => {
-      httpRequestMock.done()
-      expect(document.getElementById('info-box').textContent).toEqual('Submitted successfully')
-      done()
-    }, 100)
-  })
-
-  it('shows a failure message if the form fails to post', (done) => {
-    const httpRequestMock = mockFormKeep().reply(400, 'FAILED')
-
-    clickSubmit()
-
-    setTimeout(() => {
-      httpRequestMock.done()
-      expect(document.getElementById('info-box').textContent).toEqual('Failed to submit')
-      done()
-    }, 100)
-  })
-
-  it('posts the form with correct data', (done) => {
-    const httpRequestMock = nock('https://formkeep.com')
-    .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
-    .post('/f/f3a748fed01a', {
-      comments: 'I ❤️ FormKeep',
-      datePosted: '2018-11-28',
-      email: 'test@example.com',
-      utf8: '✓'
+  [
+    'asyncForm',
+    'post',
+    'thanksForm',
+    'redirectForm'
+  ].forEach(functionName => {
+    it(`exports ${functionName}`, () => {
+      expect(typeof FormKeep[functionName]).toEqual('function')
     })
-    .reply(200, 'OK')
-
-    clickSubmit()
-
-    setTimeout(() => {
-      httpRequestMock.done()
-      done()
-    }, 100)
   })
 })
