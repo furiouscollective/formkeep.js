392bcf37de73b54c629045f9497680f5cdb45b52
Santiago Perez Perret <perezperret@gmail.com>
2019-02-03 15:53:23 +0100

    Remove export workaround which generates warnings in Gatsby

--- index.js
+++ index.js
@@ -3,16 +3,4 @@ export { asyncForm } from './src/asyncForm'
 export { thanksForm } from './src/thanksForm'
 export { redirectForm } from './src/redirectForm'
 
-import { post } from './src/post'
-import { asyncForm } from './src/asyncForm'
-import { thanksForm } from './src/thanksForm'
-import { redirectForm } from './src/redirectForm'
-
 import './src/autoloadAsyncForms'
-
-export default {
-  post,
-  asyncForm,
-  thanksForm,
-  redirectForm
-}
