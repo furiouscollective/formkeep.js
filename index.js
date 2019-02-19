75549e6a35243023f166714326b8c273355dc812
Santiago Perez Perret <perezperret@gmail.com>
2019-02-18 21:54:44 -0400

    Remove autoload from default index

--- index.js
+++ index.js
@@ -2,5 +2,3 @@ export { post } from './src/post'
 export { asyncForm } from './src/asyncForm'
 export { thanksForm } from './src/thanksForm'
 export { redirectForm } from './src/redirectForm'
-
-import './src/autoloadAsyncForms'
